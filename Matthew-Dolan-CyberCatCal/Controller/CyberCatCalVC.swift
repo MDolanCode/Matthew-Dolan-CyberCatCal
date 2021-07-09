2dd0e912f256944e3c0d45b3db5825577466aec4
Matt Dolan <mdolan.code@gmail.com>
2021-07-09 15:34:29 -0700

    Setup IBOutlet for tableView. Setup datasource and register tableViewCell.

--- Matthew-Dolan-CyberCatCal/Controller/CyberCatCalVC.swift
+++ Matthew-Dolan-CyberCatCal/Controller/CyberCatCalVC.swift
@@ -10,11 +10,31 @@ import UIKit
 class CyberCatCalVC: UIViewController {
     
     var cyberCatManager = CyberCatManager()
+    
+    let data = Array(repeating: "Item", count: 7)
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        
+        tableView.dataSource = self
         
         cyberCatManager.getCatImage()
+    }
+}
+
+extension CyberCatCalVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        cell.textLabel?.text = self.data[indexPath.row]
+        return cell
     }
 }
 
