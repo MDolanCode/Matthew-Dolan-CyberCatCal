a0c83729b60f234d4ab66ffda0111114a1322f6d
Matt Dolan <mdolan.code@gmail.com>
2021-07-09 13:47:59 -0700

    Create CyberCatData struct and conform to Codable for JSON.

---
+++ Matthew-Dolan-CyberCatCal/Model/CyberCatData.swift
@@ -0,0 +1,12 @@
+//
+//  CyberCatData.swift
+//  Matthew-Dolan-CyberCatCal
+//
+//  Created by Matt Dolan External macOS on 2021-07-09.
+//
+
+import Foundation
+
+struct CyberCatData: Codable {
+    let url: String?
+}
