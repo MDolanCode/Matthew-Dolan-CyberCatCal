fcdffc2c220f534f923e02fc4826c3ee179264c1
Matt Dolan <mdolan.code@gmail.com>
2021-07-11 08:18:26 -0700

    Create calendar and date array. Format date. Display days. Get index of current calendar day. Set if else to check dateIndex and set highlight.

--- Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
+++ Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class CatCalVC: UIViewController {
     
-    var catManager = CatManager()
+    //    var catManager = CatManager()
     
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    var dateArray = [String]()
+    var dateIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +27,49 @@ class CatCalVC: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        catManager.getCatImage()
+        calendarHelper()
+        
+    }
+    
+    func calendarHelper() {
+        let calendar = Calendar(identifier: .iso8601)
+        let startOfWeek = calendar.dateComponents([.calendar, .yearForWeekOfYear, .weekOfYear], from: Date()).date!
+        let days = (0..<7).compactMap { calendar.date(byAdding: .day, value: $0, to: startOfWeek)  }
+        let dateFormatter: DateFormatter = {
+            let result = DateFormatter()
+            result.dateStyle = .full
+            result.timeStyle = .none
+            return result
+            
+        }()
+        let todaysIndex = days.firstIndex(of: calendar.startOfDay(for: Date()))
+        let dayDisplays = days.map { dateFormatter.string(from: $0) }
+        
+        dateArray = dayDisplays
+        dateIndex = todaysIndex ?? 0
+        print(todaysIndex!)
+        print(dayDisplays)
     }
 }
 
 extension CatCalVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return dateArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: CatCalTableViewCell.identifier, for: indexPath) as! CatCalTableViewCell
+        
+        if dateIndex == indexPath.row {
+            cell.backgroundColor = UIColor.blue
+            cell.calendarLabel.textColor = UIColor.white
+        } else {
+            cell.backgroundColor = UIColor.white
+            cell.calendarLabel.textColor = UIColor.black
+        }
+        
+        cell.calendarLabel?.text = dateArray[indexPath.row]
         
         return cell
     }
@@ -46,6 +80,16 @@ extension CatCalVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 250
     }
-    
 }
+//
+//extension CatCalTableViewCell: CatManagerDelegate {
+//    func didUpdateCatImage(_ catManager: CatManager, urlString: String) {
+//        DispatchQueue.main.async {
+//        }
+//    }
+//
+//    func didFailWithError(error: Error) {
+//        print(error)
+//    }
+//}
 
