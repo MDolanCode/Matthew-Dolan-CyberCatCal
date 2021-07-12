94b7b26203fd3858a016bea8b946d83baf739153
Matt Dolan <mdolan.code@gmail.com>
2021-07-12 13:43:42 -0700

    Delete dataArray. Refactor configureCells to take [Cat] as input for images, add text and url. Create URLSession and DispatchQueue.main for configureCells. Refactor CellForRowAt.

--- Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
+++ Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
@@ -19,7 +19,6 @@ class CatCalVC: UIViewController {
     
     // MARK: - Global Variables
     var displayables = [CellDisplayable]()
-    var dateArray = [String]()
     
     // MARK: - IBOutlets
     @IBOutlet weak var navigationBar: UINavigationBar!
@@ -29,34 +28,46 @@ class CatCalVC: UIViewController {
         super.viewDidLoad()
         
         navigationBar.barTintColor = .white
-
+        
         tableView.register(CatCalTableViewCell.nib(), forCellReuseIdentifier: CatCalTableViewCell.identifier)
         
         tableView.dataSource = self
         tableView.delegate = self
-
-        configureCells()
+        
+        // URLSession
+        URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://api.thecatapi.com/v1/images/search?limit=7")!), completionHandler: { data, _, error in
+            if let data = data {
+                let cats = try! JSONDecoder().decode([Cat].self, from: data)
+                DispatchQueue.main.async {
+                    self.configureCells(cats: cats)
+                    
+                    // Debug console to show
+                    print(cats)
+                }
+            }
+            else {
+                print(error as Any)
+            }
+        }).resume()
     }
     
     // MARK: - Configure Cells
-    func configureCells() {
+    func configureCells(cats: [Cat]) {
         let calendar = Calendar(identifier: .iso8601)
         let startOfWeek = calendar.dateComponents([.calendar, .yearForWeekOfYear, .weekOfYear], from: Date()).date!
         let days = (0..<7).compactMap { calendar.date(byAdding: .day, value: $0, to: startOfWeek)  }
-        let today = calendar.startOfDay(for: Date())
-
-        let dayDisplays = days.map { dateFormatter.string(from: $0) }
-        dateArray = dayDisplays
-        // Used for debug
-        print(dateArray)
+        let today = calendar.startOfDay(for: Date())        
         
-        displayables = days.map { date in
+        displayables = zip(days, cats).map { date, cat in
             CellDisplayable(
                 backgroundColor: date == today ? .blue : .white,
                 textColor: date == today ? .white : .black,
-                currentDay: date == today
-             )
+                text: dateFormatter.string(from: date),
+                currentDay: date == today,
+                url: cat.url
+            )
         }
+        
         tableView.reloadData()
     }
 }
@@ -64,19 +75,15 @@ class CatCalVC: UIViewController {
 // MARK: - UITableViewDataSource
 extension CatCalVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dateArray.count
+        return displayables.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: CatCalTableViewCell.identifier, for: indexPath) as! CatCalTableViewCell
         
-        let displayable = displayables[indexPath.row]
-        cell.backgroundColor = displayable.backgroundColor
-        cell.calendarLabel.textColor = displayable.textColor
-        
-        cell.calendarLabel?.text = dateArray[indexPath.row]
-        
+        let displayable = displayables[indexPath.row]        
+        cell.configure(displayable: displayable)
         
         return cell
     }
