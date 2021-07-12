c5d0ca304e467df0697bc2971360316284281216
Matt Dolan <mdolan.code@gmail.com>
2021-07-12 06:37:30 -0700

    Create CellDisplayable struct for UI.

---
+++ Matthew-Dolan-CyberCatCal/Controller/CellDisplayable.swift
@@ -0,0 +1,14 @@
+//
+//  CellDisplayable.swift
+//  Matthew-Dolan-CyberCatCal
+//
+//  Created by Matt Dolan External macOS on 2021-07-11.
+//
+
+import UIKit
+
+struct CellDisplayable {
+    let backgroundColor: UIColor
+    let textColor: UIColor
+    let currentDay: Bool
+}
