d3e28af154d20373bf8c715c34b2effce815a54a
Matt Dolan <mdolan.code@gmail.com>
2021-07-12 06:35:40 -0700

    Refactor func calendarHelper to now be confirgureCells. Included ternary operators to show whether the UI for today's date. Move date formatter outside of function. Created Displayables. Delete CatManagerDelegate. Delete if/else for dateIndex UI.

--- Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
+++ Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
@@ -9,49 +9,59 @@ import UIKit
 
 class CatCalVC: UIViewController {
     
-    //    var catManager = CatManager()
+    // MARK: - Date Formatter
+    let dateFormatter: DateFormatter = {
+        let result = DateFormatter()
+        result.dateStyle = .full
+        result.timeStyle = .none
+        return result
+    }()
     
+    // MARK: - Global Variables
+    var displayables = [CellDisplayable]()
+    var dateArray = [String]()
+    
+    // MARK: - IBOutlets
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var tableView: UITableView!
-    
-    var dateArray = [String]()
-    var dateIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationBar.barTintColor = .white
-        view.backgroundColor = .cyan
+
         tableView.register(CatCalTableViewCell.nib(), forCellReuseIdentifier: CatCalTableViewCell.identifier)
         
         tableView.dataSource = self
         tableView.delegate = self
-        
-        calendarHelper()
-        
+
+        configureCells()
     }
     
-    func calendarHelper() {
+    // MARK: - Configure Cells
+    func configureCells() {
         let calendar = Calendar(identifier: .iso8601)
         let startOfWeek = calendar.dateComponents([.calendar, .yearForWeekOfYear, .weekOfYear], from: Date()).date!
         let days = (0..<7).compactMap { calendar.date(byAdding: .day, value: $0, to: startOfWeek)  }
-        let dateFormatter: DateFormatter = {
-            let result = DateFormatter()
-            result.dateStyle = .full
-            result.timeStyle = .none
-            return result
-            
-        }()
-        let todaysIndex = days.firstIndex(of: calendar.startOfDay(for: Date()))
+        let today = calendar.startOfDay(for: Date())
+
         let dayDisplays = days.map { dateFormatter.string(from: $0) }
-        
         dateArray = dayDisplays
-        dateIndex = todaysIndex ?? 0
-        print(todaysIndex!)
-        print(dayDisplays)
+        // Used for debug
+        print(dateArray)
+        
+        displayables = days.map { date in
+            CellDisplayable(
+                backgroundColor: date == today ? .blue : .white,
+                textColor: date == today ? .white : .black,
+                currentDay: date == today
+             )
+        }
+        tableView.reloadData()
     }
 }
 
+// MARK: - UITableViewDataSource
 extension CatCalVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dateArray.count
@@ -61,35 +71,21 @@ extension CatCalVC: UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: CatCalTableViewCell.identifier, for: indexPath) as! CatCalTableViewCell
         
-        if dateIndex == indexPath.row {
-            cell.backgroundColor = UIColor.blue
-            cell.calendarLabel.textColor = UIColor.white
-        } else {
-            cell.backgroundColor = UIColor.white
-            cell.calendarLabel.textColor = UIColor.black
-        }
+        let displayable = displayables[indexPath.row]
+        cell.backgroundColor = displayable.backgroundColor
+        cell.calendarLabel.textColor = displayable.textColor
         
         cell.calendarLabel?.text = dateArray[indexPath.row]
+        
         
         return cell
     }
 }
 
+// MARK: - UITableViewDelegate
 extension CatCalVC: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 250
     }
 }
-//
-//extension CatCalTableViewCell: CatManagerDelegate {
-//    func didUpdateCatImage(_ catManager: CatManager, urlString: String) {
-//        DispatchQueue.main.async {
-//        }
-//    }
-//
-//    func didFailWithError(error: Error) {
-//        print(error)
-//    }
-//}
-
