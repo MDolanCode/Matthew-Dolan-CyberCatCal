9e8d6927aac14a234c438247a5b668ebe9ad1bea
Matt Dolan <mdolan.code@gmail.com>
2021-07-19 12:54:04 -0700

    Change navigation title font size.

--- Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
+++ Matthew-Dolan-CyberCatCal/Controller/CatCalVC.swift
@@ -28,7 +28,7 @@ class CatCalVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: 22)!]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: 18)!]
         
         tableView.register(CatCalTableViewCell.nib(), forCellReuseIdentifier: CatCalTableViewCell.identifier)
         
